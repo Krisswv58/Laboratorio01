2c3e29030af615d88a89c314575687948a5d5548
Krisswv58 <crwilsonva@est.utn.ac.cr>
2024-09-26 18:19:23 -0600

    Initial commit

---
+++ Problema02/Evaluacion.py
@@ -0,0 +1,29 @@
+class Evaluacion:
+
+    __nombreProfesor= ''
+    __nombreCurso = ''
+    __periodo = ''
+    __InstrumentoEvaluacion = ''
+
+
+def getnombreProfesor(self):
+    return self.__nombreProfesor
+def setnombreProfesor(self,nombreProfesor):
+   self.__nombreProfesor = nombreProfesor
+
+def getnombreCurso(self):
+    return self.__nombreCurso
+def setnombreCurso(self,nombreCurso):
+   self.__nombreCurso = nombreCurso
+
+def getperiodo(self):
+    return self.__periodo
+def setperiodo(self,periodo):
+    self.__periodo = periodo
+
+def getInstrumentoEvaluacion(self):
+        return self.__InstrumentoEvaluacion
+def setInstrumentoEvaluacion(self,InstrumentoEvaluacion):
+        self.__InstrumentoEvaluacion = InstrumentoEvaluacion
+
+        
